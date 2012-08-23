85ced48acb92d838a149a1abf44e66230820975d
timedwards286 <tim.edwards@nuku.com>
2012-08-23 10:13:06 -0700

    Update lib/resque/multi_queue.rb
    Don't use namespace in the queue hash.

--- lib/resque/multi_queue.rb
+++ lib/resque/multi_queue.rb
@@ -19,8 +19,9 @@ module Resque
       @redis      = redis
 
       queues.each do |queue|
-        key = @redis.is_a?(Redis::Namespace) ? "#{@redis.namespace}:" : ""
-        key += queue.redis_name
+        #TME key = @redis.is_a?(Redis::Namespace) ? "#{@redis.namespace}:" : ""
+        #TME key += queue.redis_name
+        key = queue.redis_name
         @queue_hash[key] = queue
       end
     end
